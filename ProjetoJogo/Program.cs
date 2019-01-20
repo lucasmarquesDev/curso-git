4873b0f6aadbae2179f2106c77baae72ef59b7e6
Lucas Marques <lunatec09@gmail.com>
2019-01-20 14:33:00 -0200

    Acrescentado uma mensagem de Bom dia

--- ProjetoJogo/Program.cs
+++ ProjetoJogo/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoJogo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia! ");
         }
     }
 }
